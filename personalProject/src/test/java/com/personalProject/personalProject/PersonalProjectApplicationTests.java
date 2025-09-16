04026f14a286c87efe1a62c2904c34814c5a6dbc
gabec <gabechoi03@gmail.com>
2025-09-15 20:45:33 -0500

    set up backend and db

---
+++ personalProject/src/test/java/com/personalProject/personalProject/PersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProject.personalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
